8d26047a50869ce147d9c2605fc27aea950309c8
Szafran <pawel.szafranski@snow.dog>
2020-08-14 10:42:25 +0200

    Initial commit

---
+++ components/Heading.vue
@@ -0,0 +1,10 @@
+<template src="@snowdog/alpaca-ui/src/atoms/heading/Heading.html" />
+<script src="@snowdog/alpaca-ui/src/atoms/heading/Heading.js" />
+
+<style scoped>
+@import "@snowdog/alpaca-ui/src/atoms/heading/Heading.css";
+
+.a-heading {
+  @apply font-sans
+}
+</style>
---
+++ components/Heading1.vue
@@ -0,0 +1,14 @@
+<script>
+import Heading1 from '@snowdog/alpaca-ui/src/atoms/heading/Heading.vue'
+
+export default {
+  name: 'Heading1',
+  mixins: [Heading1]
+}
+</script>
+
+<style scoped>
+  .a-heading {
+    @apply text-gray-300 mb-10
+  }
+</style>
